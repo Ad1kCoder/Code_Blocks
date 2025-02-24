2718f3a3ff56377a59d2a7a280e62e6a0a3af1c8
Ad1kCoder <nikitaperevalov92@gmail.com>
2025-02-24 20:34:34 +0300

    new

---
+++ Module_11/Practical_work/Name_generation/NG/NameGen.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "NameGen.h"
+
+void printHello(int i)
+{
+    for (int j=0;j<i;j++){
+        printf("Hello world!\n");
+    }
+}
---
+++ Module_11/Practical_work/Name_generation/NG/NameGen.h
@@ -0,0 +1,7 @@
+
+#ifndef __NameGen__
+#define __NameGen__
+ 
+void printHello(int i);
+ 
+#endif
