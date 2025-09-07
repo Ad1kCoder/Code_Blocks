1c60cb95af66ce11646228a1cbe1e3f54a3d8f08
Ad1kCoder <nikitaperevalov92@gmail.com>
2025-09-07 16:58:39 +0300

    new

--- Module_11/Practical_work/11-1/main.c
+++ Module_11/Practical_work/11-1/main.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #define RND(min, max) rand()%((max)-(min))+(min)
+
+
+#define CHOICE 0
 
 unsigned int seed;
 
@@ -15,8 +19,14 @@ typedef struct {
     char subject [50];
     int mark; // Оценка за урок, по 10-балльной системе (Белорусский стандарт)
 } Student;
+char rndSubject[50];
 
 Gender randomGender(void);
+int compSurname (const void *x, const void *y);
+float averageMark (Student *gr, int size);
+char *subject(void);
+int compMark (const void *a, const void *b);
+int whoIsSmart(Student *gr, int size);
 
 
 
@@ -31,9 +41,16 @@ int main()
     srand(seed);
     int sizeGroup = RND(15,37);
     int rndGradeLevel = RND (5,12);
+    strcpy(rndSubject, subject());
     Student group[sizeGroup];
     fillGroup (group, sizeGroup);
-    printGroup(group, sizeGroup);
+    qsort(group,sizeGroup, sizeof(*group), compSurname); // Сортировка по алфавиту
+    printGroup(group, sizeGroup, rndSubject, rndGradeLevel);
+    printf("\nAverage mark of group: %f\n", averageMark (group, sizeGroup));
+    qsort(group,sizeGroup, sizeof(*group), compMark);   // Сортировка по успеваемости
+    printGroup(group, sizeGroup, rndSubject, rndGradeLevel);
+    whoIsSmart(group, sizeGroup);
+
     return 0;
 }
 
@@ -41,7 +58,10 @@ Gender randomGender(void) {
     return RND(0, 2) == 0 ? MALE : FEMALE;
 }
 
-int printGroup (Student *gr, int size){
+int printGroup (Student *gr, int size, char *sbj, int gradeLevel){
+    printf ("\nSubject: %s\t Grade: %dA\n\n", sbj, gradeLevel);
+    printf("%-15s %-15s %-13s %-8s %-5s\n", "Surname", "Name", "Patronymic", "Gender", "Mark");
+    printf("-------------------------------------------------------------\n");
     for (int i=0; i < size; i++){
         printf("%-15.15s\t", gr[i].surname);
         printf("%-15.15s\t", gr[i].name);
@@ -55,16 +75,63 @@ int printGroup (Student *gr, int size){
 int fillGroup (Student *gr, int size){
     for (int i=0; i < size; i++){
         if (randomGender() == MALE){
+            strcpy(gr[i].surname, surnameM());
             strcpy(gr[i].name, nameM());
             strcpy(gr[i].patronymic, patronymicM());
-            strcpy(gr[i].surname, surnameM());
             gr[i].gender = MALE;
         } else {
+            strcpy(gr[i].surname, surnameF());
             strcpy(gr[i].name, nameF());
             strcpy(gr[i].patronymic, patronymicF());
-            strcpy(gr[i].surname, surnameF());
             gr[i].gender = FEMALE;
         }
         gr[i].mark = RND (3,11);
     }
 }
+
+int compSurname (const void *x, const void *y){
+    const Student *studentX = (const Student*)x;
+    const Student *studentY = (const Student*)y;
+    return strcmp(studentX ->surname, studentY ->surname);
+}
+
+float averageMark (Student *gr, int size){
+    int sum=0;
+    for (int i=0; i < size; i++){
+        sum+=gr[i].mark;
+    }
+    return ((float)sum/size);
+}
+
+char *subject(void){
+    char *subjects[] ={"Russian Language","Literature","Foreign Language","Mathematics","History","Social Studies",\
+    "Geography","Biology","Physical Education","Art","Music","Technology", "Life Safety" };
+
+    return subjects[RND(0,sizeof(subjects)/sizeof(subjects[0]))];
+}
+
+
+int compMark (const void *a, const void *b){
+        const Student *studentA = (const Student*)a;
+        const Student *studentB = (const Student*)b;
+        return ((studentB ->mark) - (studentA ->mark));
+}
+
+int whoIsSmart(Student *gr, int size){
+    int markM=0, markF=0, nM=0, nF=0;
+    for (int i=0; i < size; i++){
+        if (gr[i].gender == MALE){
+            markM+=gr[i].mark;
+            nM++;
+        }else {
+        markF+=gr[i].mark;
+        nF++;
+        }
+    }
+    if (((float)markM/nM)>((float)markF/nF)) printf("\nGuys are %f times smarter than girls\n",(((float)markM/nM)/((float)markF/nF)));
+    else printf("\nGirls are %f times smarter than guys \n",(((float)markF/nF)/((float)markM/nM)));
+}
+
+
+
+
